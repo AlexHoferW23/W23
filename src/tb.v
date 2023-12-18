c5be07634ff6ab954d31d8595efa4e4584185a96
AlexHoferW23 <carnage-infill.0r@icloud.com>
2023-12-18 10:08:07 +0100

    tb

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
+    tt_um_moving_average tt_um_moving_average (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
